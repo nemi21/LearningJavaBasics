010cdef5eec75d4b6759b76f196190fb3cd45ef1
nemi21 <48768261+nemi21@users.noreply.github.com>
2023-01-11 12:48:16 -0500

    Add files via upload

---
+++ functional-programming/src/com/in28minutes/functionalprogramming/FPNumberRunner.java
@@ -0,0 +1,35 @@
+package com.in28minutes.functionalprogramming;
+
+import java.util.List;
+
+public class FPNumberRunner {
+
+	public static void main(String[] args) {
+		List<Integer> numbers = List.of(4,6,8,13,3,15);
+		
+		/*numbers.stream()
+			.forEach(element -> System.out.println(element));*/
+		
+		int sum = fpSum(numbers);
+		//int sum = normalSum(numbers);
+		
+		System.out.println(sum);
+	}
+
+	private static int fpSum(List<Integer> numbers) {
+		return numbers.stream().
+				reduce(0,
+						(number1, number2) -> number1 + number2
+				);
+	}
+
+	private static int normalSum(List<Integer> numbers) {
+		int sum = 0;
+		for(int number:numbers) {
+			sum += number; //mutations
+		}
+		return sum;
+	}
+	
+
+}
---
+++ functional-programming/src/com/in28minutes/functionalprogramming/LambdaBehindTheScreensRunner.java
@@ -0,0 +1,71 @@
+package com.in28minutes.functionalprogramming;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+class EvenNumberPredicate implements Predicate<Integer> {
+
+	@Override
+	public boolean test(Integer number) {
+		return number%2 == 0;
+	}
+	
+}
+
+class SystemOutConsumer implements Consumer<Integer> {
+
+	@Override
+	public void accept(Integer number) {
+		System.out.println(number);
+		
+	}
+	
+}
+
+class NumberSquareMapper implements Function<Integer, Integer> {
+
+	@Override
+	public Integer apply(Integer number) {
+		return number * number;
+	}
+	
+}
+
+public class LambdaBehindTheScreensRunner {
+	
+	public static void main(String[] args) {
+		//1.Storing functions in variables
+		//2.passing function to methods <=
+		//3. Returning function from methods
+		
+		Predicate<? super Integer> evenPredicate = createEvenPredicate();
+		Predicate<? super Integer> oddPredicate = n -> n%2 ==1;
+		List.of(23,43,34,45,36,48).stream()
+		.filter(evenPredicate)
+		.map(n -> n * n)
+		.forEach(e -> System.out.println(e));
+		
+		List.of(23,43,34,45,36,48).stream()
+		.filter(new EvenNumberPredicate())
+		.map(new NumberSquareMapper())
+		.forEach(new SystemOutConsumer());
+		
+		//.map(n -> n * n)
+		//<R> Stream<R> map(Function<? super T, ? extends R> mapper);
+		//R apply(T t);
+
+		//Stream<T> filter(Predicate<? super T> predicate);
+		//boolean test(T t);
+		
+		//.forEach(e -> System.out.println(e));
+		//COnusmer<? super T> action
+		//void accept(T t);
+	}
+
+	private static Predicate<? super Integer> createEvenPredicate() {
+		return n -> n%2 ==0;
+	}
+
+}
