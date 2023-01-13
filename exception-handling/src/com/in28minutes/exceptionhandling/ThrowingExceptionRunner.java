5e6e25ff5aa7311de1deb847b1b90fc87bef7e6d
nemi21 <48768261+nemi21@users.noreply.github.com>
2023-01-13 12:30:32 -0500

    Add files via upload

---
+++ exception-handling/src/com/in28minutes/exceptionhandling/ThrowingExceptionRunner.java
@@ -0,0 +1,46 @@
+package com.in28minutes.exceptionhandling;
+
+
+class Amount {
+	private String currency;
+	private int amount;
+	
+	public Amount(String currency, int amount) {
+		super();
+		this.currency = currency;
+		this.amount = amount;
+	}
+	
+	public void add(Amount that)  {
+		if(!this.currency.equals(that.currency)) {
+			//throw new Exception("Currencies Don't Match " + this.currency + " " + that.currency);
+			
+			throw new CurrenciesDoNotMatchException("Currencies Don't Match " + this.currency + " & " + that.currency);
+		}
+		
+		this.amount = this.amount + that.amount;
+	}
+	
+	public String toString( ) {
+		return amount + " " + currency;
+	}
+	
+}
+
+class CurrenciesDoNotMatchException extends RuntimeException {
+	public CurrenciesDoNotMatchException(String msg) {
+		super(msg);
+	}
+}
+
+public class ThrowingExceptionRunner {
+	
+	public static void main(String[] args) {
+		Amount amount1 = new Amount("USD", 10);
+		Amount amount2 = new Amount("EUR", 20);
+		amount1.add(amount2);
+		System.out.println(amount1);
+
+	}
+
+}
---
+++ exception-handling/src/com/in28minutes/exceptionhandling/TryWithResourcesRunner.java
@@ -0,0 +1,18 @@
+package com.in28minutes.exceptionhandling;
+
+import java.util.Scanner;
+
+public class TryWithResourcesRunner {
+
+	public static void main(String[] args) {
+		
+		try (Scanner scanner = new Scanner(System.in)) {
+			int[] numbers = {12,3,4,5};
+			int number = numbers[21];
+		
+		}
+		
+		System.out.println("Just before closing out main");
+	}
+
+}
