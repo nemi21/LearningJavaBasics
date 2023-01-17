eb44d6f38d1da1bbb6482a5294645592c900f915
nemi21 <48768261+nemi21@users.noreply.github.com>
2023-01-17 16:13:58 -0500

    Add files via upload

---
+++ modularization-5-consumer-module/src/com/in28minutes/consumer/DirectConsumer.java
@@ -0,0 +1,21 @@
+package com.in28minutes.consumer;
+
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import com.in28minutes.sorting.algorithm.BubbleSort;
+import com.in28minutes.sorting.util.MySortingUtil;
+
+public class DirectConsumer {
+	
+	private static Logger logger = 
+			Logger.getLogger(DirectConsumer.class.getName());
+	
+	public static void main(String[] args) {
+		BubbleSort util = new BubbleSort();
+		List<String> sorted = 
+				util.sort(List.of("Ranga", "Ravi", "Sathish", "Adam", "Eve"));
+		logger.info(sorted.toString());
+	}
+}
